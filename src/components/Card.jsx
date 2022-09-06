17af6b5f1e4b9af2e93c25b3302e5e80431a198f
Jayasmita Sahu <96156277+jayasmita12@users.noreply.github.com>
2022-09-07 00:56:38 +0530

    final

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,16 +4,25 @@ import { NavbarComponent } from './Navbar'
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import { Link } from 'react-router-dom';
+import "./card.css"
 
 export const CardItem = () => {
 
     const [post, setpost] = useState([])
     const [titlee, setChangeTitle] = useState("")
     const [loading,setLoading] = useState(false)
+    const [currentPage,setCurrentPage]=useState(1);
+    const [per_page,setperpage]=useState(4)
 
-    useEffect(() => {
+    const getPost=({currentPage=1,per_page})=>{
         setLoading(true)
-        axios.get("https://jsonplaceholder.typicode.com/posts")
+        axios("https://jsonplaceholder.typicode.com/posts",{
+            method:"get",
+            params:{
+                _page:currentPage,
+                _limit:per_page
+            }
+        })
             .then(res => {
                 setpost(res.data)
                 setLoading(false)
@@ -21,13 +30,19 @@ export const CardItem = () => {
             .catch(err => {
                 console.log(err)
             })
-    }, [])
+    }
+
+    useEffect(() => {  
+        getPost({currentPage,per_page})
+    }, [currentPage,per_page])
+
+    let pagecount =Math.floor( 100/per_page)
 
     return (
         <div>
             <NavbarComponent setChangeTitle={setChangeTitle} />
             {loading ? (<h1>Loading...</h1>) :(
-            <div style={{ display: "grid", gridTemplateColumns: "auto auto auto auto", margin: "50px" }}>
+            <div className='main-div' >
                 {post.filter((value) => {
                     if (titlee === "" ) {
                         return value
@@ -38,18 +53,24 @@ export const CardItem = () => {
                 })
                     .map((e) => {
                         return (
-                            <div key={e.id} >
-                                <Card style={{ width: '18rem' }}>
+                            <div className='list-card' key={e.id} >
+                                <Card className='list-item' >
                                     <Card.Body>
                                         <Card.Title>{e.title}</Card.Title>
                                         <Card.Text>{e.body}</Card.Text>
-                                        <Link to={`/user/${e.id}`}><Button variant="primary">View User</Button></Link>
+                                        <Link to={`/user/${e.id}`}><Button variant="success">View User</Button></Link>
                                     </Card.Body>
                                 </Card>
                             </div>
                         )
                     })}
             </div>)}
+        <div style={{display:"flex",gap:"10px", justifyContent:"center",}}>
+        <Button variant="dark" disabled={currentPage===1} onClick={()=>setCurrentPage(currentPage-1)}>Prev</Button>
+        <h3>{currentPage}</h3>
+        <Button variant="dark" onClick={()=>setCurrentPage(currentPage+1)}>Next</Button>
+        </div>
+            
         </div>
     )
 
